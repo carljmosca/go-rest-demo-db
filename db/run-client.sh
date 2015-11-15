1457ae4064f82caa546f0f266a0ddab93a3fa473
Carl J Mosca <carljmosca@gmail.com>
2015-11-14 23:41:52 -0500

    updated db

---
+++ db/run-client.sh
@@ -0,0 +1,1 @@
+docker run -it --link godemo-db:mysql --name=godemo-mysql-client --rm mysql sh -c 'exec mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD"' godemo-db-client
---
+++ db/run.sh
@@ -0,0 +1,1 @@
+docker run  --name=godemo-db -d -e=MYSQL_ROOT_PASSWORD=my-secret-pw carljmosca/godemo-db
